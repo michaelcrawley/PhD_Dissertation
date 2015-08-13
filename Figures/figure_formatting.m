ebcb3cfc0bb1300ce723e1a4818f1a53aeafa979
michaelcrawley <crawley.25@osu.edu>
2015-08-12 23:20:12 -0400

    adding figures

---
+++ Figures/figure_formatting.m
@@ -0,0 +1,33 @@
+font = 'Times New Roman';
+fontsize = 14;
+boxsize  = [0 0 5 3.75];
+
+
+set(gcf,'windowstyle','normal');
+set(gcf,'paperpositionmode','auto');
+drawnow;
+set(gcf,'units','inches');
+set(gcf,'position',boxsize);
+
+children = get(gcf,'children');
+
+for n=1:length(children)
+    set(children(n),'fontname',font);
+    set(children(n),'fontsize',fontsize);
+    
+    grandchildren = get(children(n));
+    try
+        set(grandchildren.XLabel,'fontname',font);
+        set(grandchildren.YLabel,'fontname',font);
+        set(grandchildren.XLabel,'fontsize',fontsize);
+        set(grandchildren.YLabel,'fontsize',fontsize);
+    catch
+        
+    end
+end
+
+filename = input('Input file name: ','s');
+
+saveas(gcf,[filename,'.fig']);
+export_fig(filename,'-png','-transparent','-r600');
+close(gcf);
